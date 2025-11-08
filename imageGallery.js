0f12b29dfacec1286185d4d1d687e1823beb306a
Sultana Jahan Tahmina <hscsultana22@gmail.com>
2025-11-08 18:41:54 +0600

    Update imageGallery.js

--- imageGallery.js
+++ imageGallery.js
@@ -1,1 +1,14 @@
 
+/*npm create-react-app image-gallery
+cd image-gallery
+mpm start*/
+
+import React, from "react";
+
+const images= [
+  "https://images.unaplash.com/photo-1506744036-46273834b3fb",
+  "https://images.unsplash.com/photo-1519681393784-d120267933ba",
+  "https://images.unsplash.com/photo-1499084732479-de2c02d45fc4",
+  "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
+  "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+  "https://images.unsplash.com/photo-
